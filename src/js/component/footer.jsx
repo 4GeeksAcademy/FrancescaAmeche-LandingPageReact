af1d8e0311149a7d3a97aebdbcc968addb10763e
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-09-02 05:01:09 +0000

    footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = ()=> {
     return (
         <div class="container-fluid bg-dark">
-            <footer class="d-flex justify-content-center align-items-center py-4 my-4 border-top">
+            <footer class="d-flex justify-content-center py-4 my-4">
                 <p class="text-white">Copyright © Your Website 2023</p>
             </footer>
         </div>
